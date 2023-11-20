7e100b8e9d783d3a8cadd0d11f4fe20c1e0b2a89
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-11-20 18:00:26 +0700

    test caching

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import axios from 'axios';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
+
+  async getHello() {
+    const cachedValue = await this.cacheManager.get('test_key')
+    if(cachedValue){
+      console.log('hello cached value')
+      return cachedValue
+    }
+
+    const res = await axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.data)
+      // 30000 ด้านหลังคือ time-to-live (TTL)  หรือระยะเวลาที่ข้อมูลจะถูกเก็บไว้ในแคช ในที่นี้คือ 30,000 มิลลิวินาทีหรือ 30 วินาที เมื่อครบแล้ว จะลบออกเอง
+    await this.cacheManager.set('test_key', res,30000)
+    console.log('from api')
+    return res
   }
 }
